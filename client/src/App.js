bacce9f55d2ecfabe712d3df03576e740e5517bd
Michael Porter <michael.porter@utsouthwestern.edu>
2019-02-22 17:35:32 -0600

    adding a little bit of layout

--- client/src/App.js
+++ client/src/App.js
@@ -11,67 +11,22 @@ import Photos from './pages/Photos';
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
-  };
+ 
 
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar className="nav"/>
         
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/code" component={Code} />
-        <Route exact path="/photos" component={Photos} />
-
-        <Footer />
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/code" component={Code} />
+          <Route exact path="/photos" component={Photos} />
+        </div>
+        
+        <Footer className="footer"/>
       </div>
     );
   }
