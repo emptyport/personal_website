1e5746dfe56e4dd8b258971f1f0e92e33be75cf7
Michael Porter <michael.porter@utsouthwestern.edu>
2019-03-13 11:29:20 -0500

    working on styling pages now. finished about page in brutalist style

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 
+import '../styles/About.css';
+import SpiderPortrait from '../image_assets/spider_portrait.png';
+import SciencePortrait from '../image_assets/science_portrait.png';
+
 class About extends Component {
 
-    render() {
-        return (
-            <div>
-                <ul className="about-list">
-                    <li>
-                        I like coding
-                    </li>
-                    <li>
-                        I like science
-                    </li>
-                    <li>
-                        I like photography
-                    </li>
-                    <li>
-                        I like bugs
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="about">
+        <img id="spider-pic" src={SpiderPortrait} />
+        <ul className="about-list">
+          <li>
+            I like coding
+          </li>
+          <li>
+            I like science
+          </li>
+          <li>
+            I like photography
+          </li>
+          <li>
+            I like bugs
+          </li>
+        </ul>
+        <img id="science-pic" src={SciencePortrait} />
+      </div>
+    )
+  }
 
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import '../styles/Home.css';
+
 class Home extends Component {
 
   render() {
