c60412a5101d85aaf0d53417a3bb4081c7c10517
Michael Porter <michael.porter@utsouthwestern.edu>
2019-04-11 14:47:33 -0500

    working on styling

--- api/contact.js
+++ api/contact.js
@@ -1,11 +1,12 @@
 const pool = require('../utils/pool').pool;
 const nodemailer = require('nodemailer');
 const config = require('../config.json');
+const path = require('path');
 
 module.exports = function(app) {
   app.post('/api/contact', (req, res) => {
     if(req.body.trial !== '4') {
-      res.status(200).send("This appears to be a spam response because the math question was answered incorrectly");
+      res.sendFile(path.join(__dirname, '../client/_site', 'spam-contact', 'index.html'));
       return;
     }
 
@@ -31,16 +32,16 @@ module.exports = function(app) {
     }
 
     transporter.sendMail(mailOptions, function(err, info) {
-      if(err) { console.log(err); }
+      if(err) { console.log(err); return; }
       else { console.log(info); }
     });
 
     pool.query('INSERT INTO contact (first_name, last_name, email, message) VALUES ($1, $2, $3, $4)', [req.body.firstname, req.body.lastname, req.body.email, req.body.message], (error, results) => {
       if(error) {
-        res.status(401).send('There was an error processing your message, :( please try again');
+        res.sendFile(path.join(__dirname, '../client/_site', 'failed-contact', 'index.html'));
       }
 
-      res.status(200).send("Thank you for your message! I'll get back to you soon!");
+      res.sendFile(path.join(__dirname, '../client/_site', 'good-contact', 'index.html'));
 
     });   
   });
