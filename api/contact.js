80a55373179098795d053b3f2c0a92f6af737f85
Michael Porter <michael.porter@utsouthwestern.edu>
2019-04-11 13:36:48 -0500

    adding in some fonts, connecting the contact form to the backend, adding in nodemailer

--- api/contact.js
+++ api/contact.js
@@ -1,13 +1,41 @@
 const pool = require('../utils/pool').pool;
+const nodemailer = require('nodemailer');
+const config = require('../config.json');
 
 module.exports = function(app) {
   app.post('/api/contact', (req, res) => {
-    if(req.body.hidden.length > 0) {
-      res.status(200).send('Domo origato, Mr. Roboto');
+    if(req.body.trial !== '4') {
+      res.status(200).send("This appears to be a spam response because the math question was answered incorrectly");
       return;
     }
 
-    pool.query('INSERT INTO contact (first_name, last_name, email, message) VALUES ($1, $2, $3, $4)', [req.body.firstName, req.body.lastName, req.body.email, req.body.message], (error, results) => {
+    let mailTemplate = `
+      <h3>New message from ${req.body.firstname} ${req.body.lastname}</h3>
+      <p>${req.body.message}</p>
+      <p>${req.body.email}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: config.EM_USERNAME,
+        pass: config.EM_PASSWORD
+      }
+    });
+
+    const mailOptions = {
+      from: config.EM_USERNAME,
+      to: config.EMAIL,
+      subject: 'New message from michaelporter.dev',
+      html: mailTemplate
+    }
+
+    transporter.sendMail(mailOptions, function(err, info) {
+      if(err) { console.log(err); }
+      else { console.log(info); }
+    });
+
+    pool.query('INSERT INTO contact (first_name, last_name, email, message) VALUES ($1, $2, $3, $4)', [req.body.firstname, req.body.lastname, req.body.email, req.body.message], (error, results) => {
       if(error) {
         res.status(401).send('There was an error processing your message, :( please try again');
       }
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const withAuth = require('./utils/middleware');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,32 +10,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-require('./api/imageAPI')(app);
-require('./api/authentication')(app);
-require('./api/blog')(app);
-require('./api/code')(app);
 require('./api/contact')(app);
 
-// Images folder
-app.use('/images', express.static('images'));
-
-// API calls
-app.get('/checkToken', withAuth, function(req, res) {
-  res.sendStatus(200);
-});
-
 app.use(express.static(path.join(__dirname, 'client/_site')));
-/*
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    console.log('Page hit');
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-*/
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
